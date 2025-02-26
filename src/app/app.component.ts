d677cde0b7803dcfde02eee11226d72a4736420c
peacethedominator <varchaswiaman14@gmail.com>
2025-02-27 00:25:14 +0530

    Adding content to pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Aman_Varchaswi_portfolio';
+  toggleMenu(): void {
+    // Select the menu and icon elements using the document query
+    const menu = document.querySelector('.menu-links') as HTMLElement;
+    const icon = document.querySelector('.hamburger-icon') as HTMLElement;
+
+    // Check if both elements are found before toggling classes
+    if (menu && icon) {
+      menu.classList.toggle('open');
+      icon.classList.toggle('open');
+    }
+  }
 }
